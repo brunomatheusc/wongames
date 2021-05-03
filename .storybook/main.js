0e32d0ac9188b1a72e28a874613574aa10c75078
Bruno Matheus <bmatheusc@gmail.com>
2021-05-02 21:56:27 -0300

    Initial commit from Create Next App

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.mdx",
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
