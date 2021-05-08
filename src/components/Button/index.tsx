80b7bf2c54a0f428ce89229c4586c5cd706a3f02
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-05-07 22:57:26 -0300

    Add Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import * as S from './styles';
+
+export type ButtonProps = {
+	children?: ReactNode;
+	size: 'small' | 'medium' | 'large';
+}
+
+export default function Button({ children, size = 'medium' }: ButtonProps) {
+	return (
+		<S.Wrapper size={size}>
+			{ !!children && <span>{ children }</span> }
+		</S.Wrapper>
+	);
+};
