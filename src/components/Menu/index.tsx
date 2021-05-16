516ef8c8de3e4be5cd8fca296b5c44799c7256b0
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-05-15 23:11:48 -0300

    Add open and close menu function

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,16 @@
 import * as S from './styles';
-import { MdShoppingCart as ShoppingCartIcon, MdSearch as SearchIcon } from 'react-icons/md';
+import { MdShoppingCart as ShoppingCartIcon, MdSearch as SearchIcon, MdClose as CloseIcon } from 'react-icons/md';
 import { RiMenu2Fill as MenuIcon } from 'react-icons/ri';
 
 import Logo from '../../components/Logo';
+import { useState } from 'react';
 
 export default function Menu() {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<S.Wrapper>
-			<S.IconWrapper>
+			<S.IconWrapper onClick={() => setIsOpen(true)}>
 				<MenuIcon aria-label="Open menu" />
 			</S.IconWrapper>
 
@@ -24,6 +27,10 @@ export default function Menu() {
 					<ShoppingCartIcon aria-label="Open Shopping Cart" />
 				</S.IconWrapper>
 			</S.MenuGroup>
+
+			<S.MenuFull isOpen={isOpen} aria-hidden={!isOpen}>
+				<CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+			</S.MenuFull>
 		</S.Wrapper>
 	);
 };
