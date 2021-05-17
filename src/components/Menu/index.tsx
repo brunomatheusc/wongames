6ef8fcd6c863d64608f68144ff2800f3c8f339c1
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-05-16 23:27:28 -0300

    Add menufull mobile

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,6 +4,7 @@ import { RiMenu2Fill as MenuIcon } from 'react-icons/ri';
 
 import Logo from '../../components/Logo';
 import { useState } from 'react';
+import Button from 'components/Button';
 
 export default function Menu() {
 	const [isOpen, setIsOpen] = useState(false);
@@ -30,6 +31,20 @@ export default function Menu() {
 
 			<S.MenuFull isOpen={isOpen} aria-hidden={!isOpen}>
 				<CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+
+				<S.MenuNav>
+					<S.MenuLink href="#">Home</S.MenuLink>
+					<S.MenuLink href="#">Explore</S.MenuLink>
+
+				</S.MenuNav>
+
+				<S.RegisterBox>
+					<Button fullWidth size="large">Login now</Button>
+
+					<span>or</span>
+
+					<S.CreateAccount href="#" title="Sign up">Sign Up</S.CreateAccount>
+				</S.RegisterBox>
 			</S.MenuFull>
 		</S.Wrapper>
 	);
