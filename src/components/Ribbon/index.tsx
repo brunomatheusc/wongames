c5bd25ace344e2924db47346b3c1125a104670e1
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-05-19 16:39:58 -0300

    Add ribbon component

---
+++ src/components/Ribbon/index.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import * as S from './styles';
+
+export type RibbonColors = 'primary' | 'secondary';
+export type RibbonSizes = 'normal' | 'small';
+
+export type RibbonProps = {
+	children: ReactNode;
+	color?: RibbonColors;
+	size?: RibbonSizes;
+};
+
+export default function Ribbon({ children, color = 'primary', size = 'normal' }: RibbonProps) {
+	return (
+		<S.Wrapper color={color} size={size}>
+            { children }
+		</S.Wrapper>
+	);
+};
