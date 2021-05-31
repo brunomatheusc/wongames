8af2e13134232e7b912ca02e70360f4ec14092dc
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-05-31 10:53:07 -0300

    Add Slider component, react-slick lib and tests

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
 	moduleDirectories: ['node_modules', 'src'],
 	collectCoverage: true,
 	collectCoverageFrom: ['src/**/*.ts(x)?', 'src/**/stories.tsx'],
-	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+	modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 };
