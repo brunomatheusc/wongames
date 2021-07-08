9c68575e9e056ee1d39a83caf7df0d70311ad313
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-07-08 17:38:10 -0300

    [Auth] Add desktop styles

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -12,23 +12,29 @@ export default function Auth({ title, children }: AuthProps) {
 	return (
 		<S.Wrapper>
             <S.BannerBlock>
-				<Logo />
+				<S.BannerContent>
+					<Logo />
 
-				<Heading>All your favorite games in one place</Heading>
+					<div>
+						<Heading>All your favorite games in one place</Heading>
 
-				<S.Subtitle>
-					<strong>WON</strong> is the best and most complete gaming platform.
-				</S.Subtitle>
+						<S.Subtitle>
+							<strong>WON</strong> is the best and most complete gaming platform.
+						</S.Subtitle>
+					</div>
 
-				<S.Footer>Won Games 2020 @ Todos os Direitos Reservados.</S.Footer>
+					<S.Footer>Won Games 2020 @ Todos os Direitos Reservados.</S.Footer>
+				</S.BannerContent>
 			</S.BannerBlock>
 
 			<S.Content>
-				<Logo color="black" size="large" />
+				<S.ContentWrapper>
+					<Logo color="black" size="large" />
 
-				<Heading color="black" lineColor="secondary" lineLeft>{ title }</Heading>
+					<Heading color="black" lineColor="secondary" lineLeft>{ title }</Heading>
 
-				{ children }
+					{ children }
+				</S.ContentWrapper>
 			</S.Content>
 		</S.Wrapper>
 	);
