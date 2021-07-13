5f54abf5b9719520f78c1778c90e9c41b432a041
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-07-13 12:00:41 -0300

    [SignIn/SignUp] Integrating components

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,7 +1,10 @@
+import FormSignUp from "components/FormSignUp";
 import Auth from "templates/Auth";
 
 export default function SignUp() {
 	return (
-		<Auth title="Sign Up">Children</Auth>
+		<Auth title="Sign Up">
+			<FormSignUp />
+		</Auth>
 	);
 }
