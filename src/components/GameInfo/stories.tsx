8d0284b486492d8adaa5f3b379f0d1e661ae7468
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-07-16 17:18:42 -0300

    [GameInfo] Adding new component

---
+++ src/components/GameInfo/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import GameInfo, { GameInfoProps } from '.';
+
+import gameMock from './mock';
+
+export default {
+	title: 'GameInfo',
+	component: GameInfo,
+	args: gameMock,
+	parameters: {
+		backgrounds: {
+			default: 'won-dark',
+		}
+	},
+} as Meta;
+
+export const Basic: Story<GameInfoProps> = (args) => (
+	<div style={{ maxWidth: 1440, padding: 15 }}>
+		<GameInfo {...args}/>
+	</div>
+);
---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/test/helpers';
+
+import GameInfo from '.';
+
+const props = {
+	title: 'My game title',
+	description: 'Game description',
+	price: '210.00',
+};
+
+describe('<GameInfo />', () => {
+	it('should render game informations', () => {
+		renderWithTheme(<GameInfo {...props} />);
+
+		const { title, price, description } = props;
+
+		expect(screen.getByRole('heading', { name: title })).toBeInTheDocument();
+		expect(screen.getByText(`$${price}`)).toBeInTheDocument();
+		expect(screen.getByText(description)).toBeInTheDocument();
+	});
+
+	it('should render buttons', () => {
+		renderWithTheme(<GameInfo {...props} />);
+
+		expect(screen.getByRole('button', { name: /add to cart/i })).toBeInTheDocument();
+		expect(screen.getByRole('button', { name: /wishlist/i })).toBeInTheDocument();
+	});
+});
