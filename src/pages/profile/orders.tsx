1f577592acc24ecce13369564d03215d92bd9081
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-09-07 15:29:38 -0300

    [Profile/Orders] Add component to page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,10 +1,21 @@
+import OrdersLists, { OrdersListProps } from 'components/OrdersLists'
 import React from 'react'
 import Profile from 'templates/Profile'
 
-export default function ProfileOrders() {
+import mockItems from 'components/OrdersLists/mock';
+
+export default function ProfileOrders({ items }: OrdersListProps) {
 	return (
 		<Profile>
-
+			<OrdersLists items={items} />
 		</Profile>
 	)
+}
+
+export function getServerSideProps() {
+	return {
+		props: {
+			items: mockItems,
+		}
+	}
 }
