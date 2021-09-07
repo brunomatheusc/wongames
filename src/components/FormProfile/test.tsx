18c034bd2e94b68b7ddf1532354c742dff45bfe5
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-09-06 22:15:23 -0300

    [FormProfile] Add component

---
+++ src/components/FormProfile/test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/test/helpers';
+
+import FormProfile from '.';
+
+describe('<FormProfile />', () => {
+	it('should render profile form', () => {
+		renderWithTheme(<FormProfile />);
+
+		expect(screen.getByRole('heading', { name: /my profile/i })).toBeInTheDocument();
+		expect(screen.getByRole('textbox', { name: /name/i })).toBeInTheDocument();
+		expect(screen.getByRole('textbox', { name: /e-mail/i })).toBeInTheDocument();
+		expect(screen.getByPlaceholderText(/type your password/i)).toBeInTheDocument();
+		expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument();
+	});
+});
