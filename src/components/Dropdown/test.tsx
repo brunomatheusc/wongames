eade55f4a59302ad38e53c9385413e31471131ee
Bruno Matheus <bmcardoso@devgrid.co.uk>
2021-09-21 12:00:13 -0300

    [Dropdown] Add tests

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -1,11 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithTheme } from 'utils/test/helpers';
 
 import Dropdown from '.';
 
 describe('<Dropdown />', () => {
-	it('should render the Dropdown', () => {
-		const { container } = render(<Dropdown />);
+	beforeEach(() => {
+		const title = <h1 aria-label="toggle dropdown">Click here</h1>;
 
-		expect(screen.getByRole('heading', { name: /Dropdown/i })).toBeInTheDocument();
+		renderWithTheme(
+			<Dropdown title={title}>
+				<span>Content</span>
+			</Dropdown>
+		);
+	});
+
+	it('should render title', () => {
+		expect(screen.getByLabelText(/toggle dropdown/i)).toBeInTheDocument();
+	});
+
+	it('should handle open close dropdown', () => {
+		const content = screen.getByText(/content/i).parentElement!;
+
+		expect(content).toHaveStyle({ opacity: 0 });
+		expect(content.getAttribute('aria-hidden')).toBe('true');
+
+		userEvent.click(screen.getByLabelText(/toggle dropdown/i));
+
+		expect(content).toHaveStyle({ opacity: 1 });
+		expect(content.getAttribute('aria-hidden')).toBe('false');
 	});
 });
