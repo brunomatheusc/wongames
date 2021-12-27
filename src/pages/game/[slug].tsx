dcdbead2dad816f008d77631ad34533e07ffcb44
Bruno Matheus <bmatheusc@gmail.com>
2021-12-27 16:49:15 -0300

    [Game] Add query for game page

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router';
+import { initializeApollo } from 'utils/apollo';
+
 import Game, { GameTemplateProps } from 'templates/Game';
 
 import galleryMock from 'components/Gallery/mock';
@@ -5,20 +8,40 @@ import gameDetailsMock from 'components/GameDetails/mock';
 import gamesMock from 'components/GameCardSlider/mock';
 import highlightMock from 'components/Highlight/mock';
 
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames';
+import { QUERY_GAMES, QUERY_GAME_BY_SLUG } from 'graphql/queries/games';
+import { QueryGameBySlug, QueryGameBySlugVariables } from 'graphql/generated/QueryGameBySlug';
+import { GetStaticProps } from 'next';
+
+const apolloClient = initializeApollo();
+
 export default function Index(props: GameTemplateProps) {
-	return (
-		<Game {...props} />
-	);
+	const router = useRouter();
+
+	// Se a rota não tiver sido gerada ainda mostra um loading ou esqueleto
+	if (router.isFallback)
+		return null;
+
+	return <Game {...props} />;
 }
 
 export async function getStaticPaths() {
-	return {
-		paths: [{ params: { slug: 'cyberpunk-2077' }}],
-		fallback: false
-	}
+	const { data: { games } } = await apolloClient.query<QueryGames, QueryGamesVariables>({query: QUERY_GAMES, variables: { limit: 9 }});
+
+	const paths = games.map(({ slug }) => ({ params: { slug }}));
+
+	return { paths, fallback: true };
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const { data } = await apolloClient.query<QueryGameBySlug, QueryGameBySlugVariables>({ query: QUERY_GAME_BY_SLUG, variables: { slug: `${params?.slug}` }});
+
+	if (!data.games.length) {
+		return { notFound: true };
+	}
+
+	const game = data.games[0];
+
 	const descriptionHTML = `
 		<img src="https://items.gog.com/not_a_cp/ENG_product-page-addons-2020_yellow_on_black.png"><br>
 		* Exclusive Digital Comic - Cyberpunk 2077: Big City Dreams will be available in English only.
@@ -40,15 +63,23 @@ export async function getStaticProps() {
 
 	return {
 		props: {
-			cover: 'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg',
+			revalidate: 60,
+			cover: `http://localhost:1337/${game.cover?.src}`,
 			gameInfo: {
-				title: 'Cyberpunk 2077',
-				price: '59.00',
-				description: 'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obssessed with power, glamour and body modification. You play as V, mercenary outlaw going after a one-of-a-kind implant that is the key to imortality.',
+				title: game.name,
+				price: game.price,
+				description: game.short_description,
 			},
-			gallery: galleryMock,
-			description: descriptionHTML,
-			details: gameDetailsMock,
+			gallery: game.gallery,
+			description: game.description,
+			details: {
+				developr: game.developers[0].name,
+				releaseDate: game.release_date,
+				platforms: game.platforms.map((platform) => platform.name),
+				publisher: game.publisher?.name || '',
+				rating: game.rating,
+				genres: game.categories.map((category) => category.name),
+			},
 			upcommingGames: gamesMock,
 			upcommingHighlight: highlightMock,
 			recommendedGames: gamesMock,
