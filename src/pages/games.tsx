ae37730fbdba4da4608e1dc427b956603c2522b0
Bruno Matheus <bmatheusc@gmail.com>
2022-01-09 10:03:42 -0300

    [Games] Adding useQuery hook from apollo

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -7,13 +7,16 @@ import filterItemsMock from 'components/ExploreSidebar/mock';
 import { initializeApollo } from "utils/apollo";
 
 export default function GamesPage(props: GameTemplateProps) {
-	return (<GamesTemplate {...props} />);
+	return <GamesTemplate {...props} />;
 }
 
 export async function getStaticProps() {
 	const apolloClient = initializeApollo();
 
-	const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({ query: QUERY_GAMES, variables: { limit: 9 }});
+	const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+		query: QUERY_GAMES,
+		variables: { limit: 9 }
+	});
 
 	const games = data.games.map(({ name, slug, developers, cover, price }) => ({
 		title: name,
--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -3,16 +3,22 @@ import Base from 'templates/Base';
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar';
 import GameCard, { GameCardProps } from 'components/GameCard';
 
-import * as S from './styles';
-import { Grid } from 'components/Grid';
 import { KeyboardArrowDown } from '@styled-icons/material-outlined';
+import { useQuery } from '@apollo/client';
+import { QUERY_GAMES } from 'graphql/queries/games';
+
+import { Grid } from 'components/Grid';
+import * as S from './styles';
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames';
 
 export type GameTemplateProps = {
 	games?: GameCardProps[];
 	filterItems: ItemProps[];
 };
 
-export default function GamesTemplate({ games, filterItems }: GameTemplateProps) {
+export default function GamesTemplate({ filterItems }: GameTemplateProps) {
+	const { data, loading, error } = useQuery<QueryGames, QueryGamesVariables>(QUERY_GAMES, { variables: { limit: 15 }});
+
 	function handleFilter() {
 		return;
 	}
@@ -28,8 +34,15 @@ export default function GamesTemplate({ games, filterItems }: GameTemplateProps)
 
 				<section>
 					<Grid>
-						{games && games.map((game, index) => (
-							<GameCard key={`${game.title}-${index}`} {...game} />
+						{data?.games && data.games.map(({ slug, name, developers, price, cover }, index) => (
+							<GameCard
+								key={`${slug}-${index}`}
+								title={name}
+								slug={slug}
+								developer={developers[0].name}
+								img={`http://localhost:1337${cover?.url}`}
+								price={price}
+							/>
 						))}
 					</Grid>
 
