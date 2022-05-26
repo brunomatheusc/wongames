c9518d8cf4d27077d950b730c9a09ff09fda425e
brunomatheusc <bmatheusc@gmail.com>
2022-05-26 17:20:08 -0300

    [PaymentForm] Add api integration

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,11 +1,14 @@
 import { FormEvent, useEffect, useState } from 'react';
 import { Session } from 'next-auth';
+import { useRouter } from 'next/router';
+
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import { StripeCardElementChangeEvent } from '@stripe/stripe-js';
+import { PaymentIntent, StripeCardElementChangeEvent } from '@stripe/stripe-js';
+
 import { ShoppingCart, ErrorOutline } from '@styled-icons/material-outlined';
 
 import { useCart } from 'hooks/use-cart';
-import { createPaymentIntent } from 'utils/stripe/methods';
+import { createPayment, createPaymentIntent } from 'utils/stripe/methods';
 
 import Button from 'components/Button';
 import Heading from 'components/Heading';
@@ -18,7 +21,9 @@ type PaymentFormProps = {
 };
 
 export default function PaymentForm({ session }: PaymentFormProps) {
-	const { items } = useCart();
+	const { items, clearCart } = useCart();
+	const { push } = useRouter();
+
 	const [error, setError] = useState<string | null>(null);
 	const [loading, setLoading] = useState<boolean>(false);
 	const [disabled, setDisabled] = useState<boolean>(true);
@@ -63,21 +68,36 @@ export default function PaymentForm({ session }: PaymentFormProps) {
 
 		setLoading(true);
 
-		const payload = await stripe!.confirmCardPayment(clientSecret, {
-			payment_method: {
-				card: elements!.getElement(CardElement)!,
+		if (freeGames) {
+			await saveOrder();
+		} else {
+			const payload = await stripe!.confirmCardPayment(clientSecret, {
+				payment_method: {
+					card: elements!.getElement(CardElement)!,
+				}
+			});
+
+			if (payload.error){
+				setError(`Payment failed ${payload.error.message}`);
+				setLoading(false);
+
+				return;
 			}
-		});
 
-		if (payload.error){
-			setError(`Payment failed ${payload.error.message}`);
-			setLoading(false);
-
-			return;
+			await saveOrder(payload.paymentIntent);
 		}
 
+		push('/success');
+
+		clearCart();
 		setError(null);
 		setLoading(false);
+	}
+
+	async function saveOrder(paymentIntent?: PaymentIntent) {
+		const data = await createPayment({ items, paymentIntent, token: session.jwt as string });
+
+		return data;
 	}
 
 	return (
--- src/utils/stripe/methods.ts
+++ src/utils/stripe/methods.ts
@@ -1,19 +1,52 @@
+import { PaymentIntent } from '@stripe/stripe-js';
 import { CartItem } from 'hooks/use-cart';
+
+type FetcherParams = {
+	url: string;
+	body: string;
+	token: string;
+};
 
 type PaymentIntentParams = {
 	token: string;
 	items: CartItem[];
 };
 
-export async function createPaymentIntent({ token, items }: PaymentIntentParams) {
-	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders/create-payment-intent`, {
+type CreatePaymentParams = {
+	items: CartItem[];
+	paymentIntent?: PaymentIntent;
+	token: string;
+};
+
+async function fetcher({ url, body, token }: FetcherParams) {
+	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 			'Authorization': `Bearer ${token}`,
 		},
-		body: JSON.stringify({ cart: items }),
+		body
 	});
 
 	return await response.json();
+};
+
+export async function createPaymentIntent({ token, items }: PaymentIntentParams) {
+	return fetcher({
+		url: '/orders/create-payment-intent',
+		body: JSON.stringify({ cart: items }),
+		token
+	});
+}
+
+export async function createPayment({ items, paymentIntent, token }: CreatePaymentParams) {
+	return fetcher({
+		url: '/orders',
+		body: JSON.stringify({
+			cart: items,
+			paymentIntentId: paymentIntent?.id,
+			paymentMethod: paymentIntent?.payment_method,
+		}),
+		token
+	});
 }
