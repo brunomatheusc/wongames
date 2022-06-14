8f66cf31d8c20125ff5fe5eca7be00c2e6e574e5
brunomatheusc <bmatheusc@gmail.com>
2022-06-14 18:16:20 -0300

    [Cypress] Adding tests for stripe on checkou page

--- cypress/e2e/checkout.ts
+++ cypress/e2e/checkout.ts
@@ -60,5 +60,62 @@ describe('Checkout', () => {
 		});
 	});
 
-	describe('Paid Games', () => {});
+	describe.only('Paid Games', () => {
+		before(() => {
+			user = createUser();
+		})
+
+		it('should buy paid games', () => {
+			// cy.visit('/sign-in');
+			// cy.signIn();
+
+			cy.visit('/sign-up');
+			cy.signUp(user);
+
+			cy.wait(2000);
+			cy.url().should('eq', `${Cypress.config().baseUrl}/`);
+
+			cy.findByRole('link', { name: /explore/i }).click();
+			cy.wait(1500);
+			cy.url().should('eq', `${Cypress.config().baseUrl}/games`);
+
+			cy.findByText(/highest to lowest/i).click();
+			cy.url().should('contain', 'sort=price%3Adesc');
+
+			cy.addToCartByIndex(0);
+
+			cy.findAllByLabelText(/cart items/i)
+				.first()
+				.should('have.text', 1)
+				.click();
+
+			cy.getByDataCy('cart-list').within(() => {
+				cy.findByText(/buy it now/i).click();
+				cy.wait(3500);
+			});
+
+			cy.findByRole('button', { name: /buy now/i }).should('have.attr', 'disabled');
+
+			cy.fillElementsInput('cardNumber', '4242424242424242');
+			cy.fillElementsInput('cardExpiry', '1040');
+			cy.fillElementsInput('cardCvc', '104');
+
+			cy.findByRole('button', { name: /buy now/i }).click();
+			cy.wait(6000);
+
+			cy.url().should('eq', `${Cypress.config().baseUrl}/success`);
+			cy.findByRole('heading', { name: /Your purchase was successful/i }).should('exist');
+		});
+
+		it('should show games in order page', () => {
+			cy.visit('/profile/orders');
+			cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/orders`);
+
+			cy.signIn(user.email, user.password);
+			cy.wait(3500);
+			cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/orders`);
+
+			cy.getByDataCy('game-item').should('have.length', 1);
+		});
+	});
 });
